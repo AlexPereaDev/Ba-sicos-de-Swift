50a9be323459b4ee40b4190139e741cdc1de3bc9
Alejandro Perea Navarrete <Alex@MacBook-Pro-de-Alejandro.local>
2014-12-10 15:31:20 +0100

    Initial Commit

---
+++ Básicos de Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Básicos de Swift
+//
+//  Created by Alejandro Perea Navarrete on 10/12/14.
+//  Copyright (c) 2014 Alejandro Perea Navarrete. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
